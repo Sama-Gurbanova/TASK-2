36e8b0130465d7b34d3b0b89cbeb187398bcae8d
Sama-Gurbanova <smaqurbanova456@gmail.com>
2024-08-30 20:50:21 +0400

    task 2

---
+++ js/main.js
@@ -0,0 +1,46 @@
+const btn1 = document.querySelector('.addBtn');
+const btn2 = document.querySelector('.removeBtn');
+const div = document.querySelector('.div');
+const arr = ["JS" , "HTML", "CSS"];
+const list = document.createElement("ul");
+document.body.appendChild(list);
+
+let index = 0
+
+function Display () {
+   
+   if (index < arr.length){
+         const item = document.createElement("li");
+         item.textContent = arr[index];
+         list.appendChild(item);
+         index++
+   }
+}
+
+function Remove () {
+   
+   if (index > 0) {
+      list.removeChild(list.lastChild); 
+      index--;
+  }
+}
+
+
+btn1.addEventListener('click' , () =>{
+  Display()
+})
+
+btn2.addEventListener('click' , () =>{
+  Remove()
+})
+
+
+
+
+
+
+
+
+
+
+
